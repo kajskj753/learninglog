c98d30ce8413994780cc35dcdb6e9cb6389488cb
sss <976436795@qq.com>
2019-03-29 16:58:00 +0800

    主页

--- learning_log/MyApp/urls.py
+++ learning_log/MyApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import re_path
 from . import views
-
+app_name='MyApp'
 urlpatterns = [
 
     re_path(r'^$',views.index,name = 'index'),
+    re_path(r'^topics/(?P<topic_id>\d+)$',views.topics,name = 'topics'),
 ]
--- learning_log/MyApp/views.py
+++ learning_log/MyApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Topic,Entry
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'MyApp/index.html')
+
+
+def topics(request,topic_id):
+    """显示所有主题"""
+
+    topics = Topic.objects.get(id=topic_id)
+    context = {'topics':topics}
+    return render(request,'MyApp/topic.html',context)
